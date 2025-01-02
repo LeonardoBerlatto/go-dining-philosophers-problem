d303acf988e3642563cf7eb1d1c4d195fd43eca1
LeonardoBerlatto <leoberlatto@gmail.com>
2025-01-02 15:05:41 -0300

    Update Philosopher struct with fork pointers

--- internal/philosopher/philosopher.go
+++ internal/philosopher/philosopher.go
@@ -1,7 +1,65 @@
 package philosopher
 
+import (
+	"dining-philosophers/internal/fork"
+	logger "dining-philosophers/pkg/log"
+	"math/rand"
+	"sync"
+	"time"
+)
+
 type Philosopher struct {
 	Name      string
-	LeftFork  int
-	RightFork int
+	LeftFork  *fork.Fork
+	RightFork *fork.Fork
+}
+
+const TimesToEat = 3
+
+var log = logger.GetLogger()
+
+func NewPhilosopher(name string, left, right *fork.Fork) *Philosopher {
+	return &Philosopher{Name: name, LeftFork: left, RightFork: right}
+}
+
+func (p *Philosopher) Dine(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for range TimesToEat {
+		p.eat()
+		p.think()
+	}
+	log.Info("Philosopher " + p.Name + " has finished dining")
+}
+
+func (p *Philosopher) think() {
+	log.Info("Philosopher " + p.Name + " is thinking")
+	time.Sleep(getThinkingTime())
+}
+
+func (p *Philosopher) eat() {
+	if p.LeftFork.ID > p.RightFork.ID {
+		p.RightFork.Lock()
+		log.Info("Philosopher " + p.Name + " has right fork")
+		p.LeftFork.Lock()
+		log.Info("Philosopher " + p.Name + " has left fork")
+	} else {
+		p.LeftFork.Lock()
+		log.Info("Philosopher " + p.Name + " has left fork")
+		p.RightFork.Lock()
+		log.Info("Philosopher " + p.Name + " has right fork")
+	}
+
+	log.Info("Philosopher " + p.Name + " is eating")
+	time.Sleep(getEatingTime())
+
+	p.LeftFork.Unlock()
+	p.RightFork.Unlock()
+}
+
+func getEatingTime() time.Duration {
+	return time.Duration(rand.Intn(1000)+1) * time.Millisecond
+}
+
+func getThinkingTime() time.Duration {
+	return time.Duration(rand.Intn(1000)+1) * time.Millisecond
 }
